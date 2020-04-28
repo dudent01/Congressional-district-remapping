50f0e3863414d21f5d8281c2812a132cf62597b1
Raymond Zheng <raymond.zheng@stonybrook.edu>
2020-04-27 22:17:43 -0400

    get presidential16 election data and jsonignored some hibernate garbage

---
+++ Server/src/test/java/Gators/api/StateControllerTest.java
@@ -0,0 +1,20 @@
+package Gators.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateControllerTest {
+
+    @Test
+    void getAllStates() {
+    }
+
+    @Test
+    void getStateById() {
+    }
+
+    @Test
+    void getStateByAbbr() {
+    }
+}
