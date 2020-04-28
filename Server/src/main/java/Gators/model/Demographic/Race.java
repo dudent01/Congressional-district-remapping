244b7b56268e770610cf36b563e6c6e40cb0467b
Raymond Zheng <raymond.zheng@stonybrook.edu>
2020-04-28 18:16:58 -0400

    test cases with new properties

--- Server/src/main/java/Gators/model/Demographic/Race.java
+++
@@ -1,9 +0,0 @@
-package Gators.model.Demographic;
-
-public enum Race {
-    WHITE,
-    BLACK,
-    ASIAN,
-    HISPANIC,
-    OTHER
-}
---
+++ Server/src/test/resources/TestDemographicData.sql
@@ -0,0 +1,2 @@
+INSERT INTO precinct(id, demographic_id) VALUES (999999, 999999);
+INSERT INTO demographic VALUES (999999, 200, 200, 50, 20, 10);
