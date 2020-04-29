72717576da333a616bacafe0be44fa349d6f0877
Raymond Zheng <raymond.zheng@stonybrook.edu>
2020-04-28 22:33:27 -0400

    formatting changes

--- Server/src/test/resources/TestDemographicData.sql
+++ Server/src/test/resources/TestDemographicData.sql
@@ -1,2 +1,4 @@
-INSERT INTO precinct(id, demographic_id) VALUES (999999, 999999);
-INSERT INTO demographic VALUES (999999, 200, 200, 50, 20, 10);
+INSERT INTO precinct(id, demographic_id)
+VALUES (999999, 999999);
+INSERT INTO demographic
+VALUES (999999, 200, 200, 50, 20, 10);
