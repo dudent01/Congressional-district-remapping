0ea96a32fe1f823de1dc044f2ec03755d4ebdc55
Benny Feng <benny.feng@stonybrook.edu>
2020-05-07 18:06:14 -0400

    Add changes for filtering out errors from geojson

--- Preprocessing/load_into_database/filter.py
+++ Preprocessing/load_into_database/filter.py
@@ -3,15 +3,15 @@ import json
 with open('../Utah.json') as f:
     data = json.load(f)
 
-overlapping_precincts = {
+enclosed_precincts = {
     "type": "FeatureCollection",
     "features": []
 }
 
 for precinct in data["features"]:
     if "errors" in precinct["properties"] and precinct["properties"]["errors"]:
-        if any("Overlapping precincts" in e for e in precinct["properties"]["errors"]):
-            overlapping_precincts["features"].append(precinct)
+        if any("Enclosed precinct" in e for e in precinct["properties"]["errors"]):
+            enclosed_precincts["features"].append(precinct)
 
-with open('Overlapping_Utah.json', 'w') as f:
-    json.dump(overlapping_precincts, f)
+with open('Enclosed_Utah.json', 'w') as f:
+    json.dump(enclosed_precincts, f)
