b5653f2209cd0bd497c9a9a84295de1b1f66e9c1
Benny Feng <benny.feng@stonybrook.edu>
2020-05-12 18:30:01 -0400

    Change cors config and also do add/delete neighbors

--- frontend/src/actions/precinctActions.js
+++ frontend/src/actions/precinctActions.js
@@ -1,6 +1,7 @@
 import {
   DELETE_PRECINCTS, REQUEST_PRECINCTS, RECEIVE_PRECINCTS, REQUEST_SELECTED_PRECINCT_DATA, RECIEVE_SELECTED_PRECINCT_DATA,
-  SET_SELECTED_PRECINCT
+  SET_SELECTED_PRECINCT,
+  ADD_NEIGHBOR, DELETE_NEIGHBOR, MERGE_PRECINCTS
 } from './types';
 import axios from 'axios';
 
@@ -8,7 +9,7 @@ import axios from 'axios';
 export const deletePrecincts = () => ({ type: DELETE_PRECINCTS })
 export const requestPrecincts = () => ({ type: REQUEST_PRECINCTS })
 export const requestSelectedPrecinctData = () => ({ type: REQUEST_SELECTED_PRECINCT_DATA })
-export const recieveSelectedPrecinctData = election => ({ type: RECIEVE_SELECTED_PRECINCT_DATA, election })
+export const recieveSelectedPrecinctData = (election, neighbors) => ({ type: RECIEVE_SELECTED_PRECINCT_DATA, election, neighbors })
 export const setSelectedPrecinct = precinct => {
   return {
     type: SET_SELECTED_PRECINCT,
@@ -22,6 +23,12 @@ export const recievePrecincts = (precincts, geojson) => {
     geojson
   }
 }
+export const addNeighbor = (neighborId) => {
+  return { type: ADD_NEIGHBOR, neighborId }
+}
+export const deleteNeighbor = (neighborId) => {
+  return { type: DELETE_NEIGHBOR, neighborId }
+}
 
 // Asynchronous actions
 export const fetchPrecinctData = (id, election, precincts) => {
@@ -29,8 +36,13 @@ export const fetchPrecinctData = (id, election, precincts) => {
     const selectedPrecinct = precincts.find(p => p.id === id)
     dispatch(setSelectedPrecinct(selectedPrecinct))
     dispatch(requestSelectedPrecinctData())
-    const { data } = await axios.get(process.env.REACT_APP_API_URL + `/api/precinct/${id}/${election}`);
-    dispatch(recieveSelectedPrecinctData(data[0]));
+    let values = await Promise.all([
+      axios.get(process.env.REACT_APP_API_URL + `/api/precinct/${id}/${election}`),
+      axios.get(process.env.REACT_APP_API_URL + `/api/precinct/${id}/neighbors`)
+    ])
+    let data = values[0].data[0];
+    let neighbors = values[1].data;
+    dispatch(recieveSelectedPrecinctData(data, neighbors));
   }
 }
 export const fetchPrecinctsByState = (abbr) => {
@@ -63,5 +75,17 @@ export const updatePrecinctGeojson = (id, geojson) => {
   return async (dispatch) => {
     await axios.put(process.env.REACT_APP_API_URL + `/api/precinct/${id}/geojson`, geojson)
     // dispatch({ type: SET_PRECINCT_GEOJSON, geojson, id })
+  }
+}
+export const addNeighborAsync = (id, neighborId) => {
+  return async (dispatch) => {
+    await axios.patch(process.env.REACT_APP_API_URL + `/api/precinct/${id}/${neighborId}`)
+    dispatch(addNeighbor(neighborId))
+  }
+}
+export const deleteNeighborAsync = (id, neighborId) => {
+  return async (dispatch) => {
+    await axios.delete(process.env.REACT_APP_API_URL + `/api/precinct/${id}/${neighborId}`)
+    dispatch(deleteNeighbor(neighborId))
   }
 }
