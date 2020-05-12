ba21de4efcc8c8533834700b07b40b2ec4bcbf91
Raymond Zheng <raymond.zheng@stonybrook.edu>
2020-05-12 16:47:45 -0400

    logs stuff

---
+++ Server/src/main/java/Gators/repository/LogRepository.java
@@ -0,0 +1,9 @@
+package Gators.repository;
+
+import Gators.model.Error.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogRepository extends JpaRepository<Log, Long> {
+}
--- Server/src/main/java/Gators/service/PrecinctService.java
+++ Server/src/main/java/Gators/service/PrecinctService.java
@@ -1,24 +1,26 @@
 package Gators.service;
 
 import Gators.model.Demographic.Demographic;
+import Gators.model.Error.Log;
 import Gators.model.Precinct;
+import Gators.repository.LogRepository;
 import Gators.repository.PrecinctRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class PrecinctService {
     private final PrecinctRepository precinctRepository;
+    private final LogRepository logRepository;
 
     @Autowired
-    public PrecinctService(PrecinctRepository precinctRepository) {
+    public PrecinctService(PrecinctRepository precinctRepository, LogRepository logRepository) {
         this.precinctRepository = precinctRepository;
+        this.logRepository = logRepository;
     }
 
     @Transactional
@@ -26,11 +28,27 @@ public class PrecinctService {
         Precinct precinct1 = precinctRepository.findById(id1).orElse(null);
         Precinct precinct2 = precinctRepository.findById(id2).orElse(null);
 
+        Log log1 = new Log();
+        log1.setLogDate(new Date());
+        log1.setPrecinct(precinct1);
+        log1.setOldData(precinct1.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+
+        Log log2 = new Log();
+        log2.setLogDate(new Date());
+        log2.setPrecinct(precinct2);
+        log2.setOldData(precinct2.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+
         precinct1.getNeighbors().add(precinct2);
         precinct2.getNeighbors().add(precinct1);
 
+        log1.setNewData(precinct1.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+        log2.setNewData(precinct2.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+
         precinctRepository.save(precinct1);
         precinctRepository.save(precinct2);
+
+        logRepository.save(log1);
+        logRepository.save(log2);
     }
 
     public Set<Precinct> getPrecinctsByStateAbbr(String stateAbbr) {
@@ -52,8 +70,19 @@ public class PrecinctService {
     @Transactional
     public void editGeojsonById(long id, String geojson) {
         Precinct precinct = precinctRepository.findById(id).orElse(null);
+
+        Log log = new Log();
+        log.setLogDate(new Date());
+        log.setPrecinct(precinct);
+        log.setOldData(precinct.getGeojson());
+
         precinct.setGeojson(geojson);
+
+        log.setNewData(precinct.getGeojson());
+
         precinctRepository.save(precinct);
+
+        logRepository.save(log);
     }
 
     @Transactional
@@ -61,10 +90,26 @@ public class PrecinctService {
         Precinct precinct1 = precinctRepository.findById(id1).orElse(null);
         Precinct precinct2 = precinctRepository.findById(id2).orElse(null);
 
+        Log log1 = new Log();
+        log1.setLogDate(new Date());
+        log1.setPrecinct(precinct1);
+        log1.setOldData(precinct1.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+
+        Log log2 = new Log();
+        log2.setLogDate(new Date());
+        log2.setPrecinct(precinct2);
+        log2.setOldData(precinct2.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+
         precinct1.getNeighbors().remove(precinct2);
         precinct2.getNeighbors().remove(precinct1);
 
+        log1.setNewData(precinct1.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+        log2.setNewData(precinct2.getNeighbors().stream().map(Precinct::getCName).collect(Collectors.toSet()).toString());
+
         precinctRepository.save(precinct1);
         precinctRepository.save(precinct2);
+
+        logRepository.save(log1);
+        logRepository.save(log2);
     }
 }
