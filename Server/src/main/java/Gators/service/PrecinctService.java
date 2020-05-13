c54624a59d8a788eda1c9d04c15023509ccfef9f
Benny Feng <benny.feng@stonybrook.edu>
2020-05-13 06:28:45 -0400

    Merge geojson now working

--- Server/src/main/java/Gators/service/PrecinctService.java
+++ Server/src/main/java/Gators/service/PrecinctService.java
@@ -5,10 +5,22 @@ import Gators.model.Error.Log;
 import Gators.model.Precinct;
 import Gators.repository.LogRepository;
 import Gators.repository.PrecinctRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.json.JSONParser;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryCollection;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.operation.buffer.BufferOp;
+import org.locationtech.jts.operation.buffer.BufferParameters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.io.geojson.GeoJsonReader;
+import org.locationtech.jts.io.geojson.GeoJsonWriter;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -107,8 +119,58 @@ public class PrecinctService {
         Precinct precinct1 = precinctRepository.findById(id1).orElse(null);
         Precinct precinct2 = precinctRepository.findById(id2).orElse(null);
 
-        GeometryFactory obj;
+        JsonParser springParser = JsonParserFactory.getJsonParser();
+        Map<String, Object> precinct1Map = springParser.parseMap(precinct1.getGeojson());
+        Map<String, Object> precinct2Map = springParser.parseMap(precinct2.getGeojson());
 
+        String precinct1Json = null;
+        String precinct2Json = null;
+        try {
+            precinct1Json = new ObjectMapper().writeValueAsString(precinct1Map.get("geometry"));
+            precinct2Json = new ObjectMapper().writeValueAsString(precinct2Map.get("geometry"));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        Collection<Geometry> geometryList = new LinkedList<>();
+        GeoJsonReader geoJsonReader = new GeoJsonReader();
+        Geometry g1 = null, g2 = null;
+        try {
+            g1 = inflate(geoJsonReader.read(precinct1Json));
+            g2 = inflate(geoJsonReader.read(precinct2Json));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Collections.addAll(geometryList, g1, g2);
+        GeometryFactory geometryFactory = new GeometryFactory();
+        Object obj = geometryFactory.buildGeometry(geometryList);
+        GeometryCollection geometryCollection = (GeometryCollection) obj;
+        Geometry union = geometryCollection.union();  //perform union
+        String str = new GeoJsonWriter().write(deflate(union));
+        precinct1Map.put("geometry", springParser.parseMap(str));
+
+        String newGeojson = null;
+        try {
+            newGeojson = new ObjectMapper().writeValueAsString(precinct1Map);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         return precinct1;
+    }
+    private Geometry deflate(Geometry geom) {
+        BufferParameters bufferParameters = new BufferParameters();
+        bufferParameters.setEndCapStyle(BufferParameters.CAP_ROUND);
+        bufferParameters.setJoinStyle(BufferParameters.JOIN_MITRE);
+        Geometry buffered = BufferOp.bufferOp(geom, -.0001, bufferParameters);
+        buffered.setUserData(geom.getUserData());
+        return buffered;
+    }
+    private Geometry inflate(Geometry geom) {
+        BufferParameters bufferParameters = new BufferParameters();
+        bufferParameters.setEndCapStyle(BufferParameters.CAP_ROUND);
+        bufferParameters.setJoinStyle(BufferParameters.JOIN_MITRE);
+        Geometry buffered = BufferOp.bufferOp(geom,.0001, bufferParameters);
+        buffered.setUserData(geom.getUserData());
+        return buffered;
     }
 }
